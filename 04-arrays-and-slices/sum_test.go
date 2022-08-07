cb543cb8879823c628f954bde1c535df72d2aee6
Mauro Franchini <mau.mdq@gmail.com>
2022-08-07 09:58:51 +0200

    Refactor test and add another func

--- 04-arrays-and-slices/sum_test.go
+++ 04-arrays-and-slices/sum_test.go
@@ -1,6 +1,9 @@
 package arraysAndSlices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -28,3 +31,39 @@ func TestSum(t *testing.T) {
 
 }
 
+func TestSumAll(t *testing.T) {
+	numbers1 := []int{1, 2}
+	numbers2 := []int{0, 9}
+
+	got := SumAll(numbers1, numbers2)
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Got %v want %v", got, want)
+	}
+
+}
+
+func TestSummAllTails(t *testing.T) {
+
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Got %v want %v", got, want)
+		}
+	}
+
+	t.Run("Make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("Safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		
+		checkSums(t, got, want)
+	})
+}
